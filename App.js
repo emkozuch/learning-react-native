fc957fc87b496aa55f3c0bb993d29c7702683e2b
emkozuch <emkozuch@gmail.com>
2020-02-13 21:55:12 +0100

    styling

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Exercitation ullamco duis in incididunt quis ea enim ut eiusmod.</Text>
+        <Text>Exercitation ullamco duis in incididunt quis ea enim ut eiusmod.</Text>
+        <Text>Exercitation ullamco duis in incididunt quis ea enim ut eiusmod.</Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +23,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
